e64f7356ed7d4ba201f9746585625ab42454d909
Sharon005 <sharonts123@gmail.com>
2021-04-09 20:57:43 +0530

    first commit

---
+++ store/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Create your models here.
+
+class Customer(models.Models):
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True, blank=True)
+    name = models.CharField(max_length=100, null=True)
+    email = models.CharField(max_length==100, null=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Product(models.Models):
+    name = models.CharField(max_length=100, null=True)
+    price = models.FloatField()
+    digital = models.BooleanField(default=False, null=True, blank=False)
+    
+    def __str__(self):
+        return self.name
+    
+
+class Order(models.Models):
+    customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, blank=True, null=True)
+    date_ordered = models.DateTimeField(auto_now_add=True)
+    complete = models.BooleanField(default=False, null=True, blank=False)
+    transaction_id = models.CharField(max_length=100, null=True)
+
+    def __str__(self):
+        return str(self.id)
+
+class OrderItems(models.Models):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField
